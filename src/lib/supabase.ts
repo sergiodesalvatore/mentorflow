2c50b7420177304bc3f507b04bd447170c9c06f1
Sergio De Salvatore <sergi@Mac.homenet.telecomitalia.it>
2025-12-18 10:31:35 +0100

    fix: update supabase anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 // Hardcoded credentials to simple deployment for user
 // NOTE: Ideally these should be in .env, but hardcoding here to unblock deployment issues with Vercel UI
 const supabaseUrl = 'https://oreumuycaizvlxoudhrg.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yZXVtdXljYWl6dmx4b3VkaHJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwMTAxMTUsImV4cCI6MjA4MTU4NjExNX0.SdMUYou9PIOFMgtVTuE-HhvT3GcrUtaxtVLC79-M_9cR';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yZXVtdXljYWl6dmx4b3VkaHJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwMTAxMTUsImV4cCI6MjA4MTU4NjExNX0.SdMUYou9PIOFMgtVTuE-HhvT3GcrUtaxtVLC79-M_9c';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
